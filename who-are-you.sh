25b42e6e8659ad622b20aa4dcce391dc60445f2b
aid000 <1efori9@gmail.com>
2019-10-15 09:12:54 +0600

    Batman

---
+++ who-are-you.sh
@@ -0,0 +1,2 @@
+curl https://raw.githubusercontent.com/kigiri/superhero-api/master/api/all.json | jq --arg ID_num "$HERO_ID" '.[] | select( .id ==($ID_num|tonumber)) | .connections.relatives' | sed 's/"//g'
+
