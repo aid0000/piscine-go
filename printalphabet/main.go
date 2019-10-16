bb125f2c91d95ea1215d7ad498e1ff40efdaec6c
aid000 <1efori9@gmail.com>
2019-10-16 13:11:14 +0600

    s

--- printalphabet/main.go
+++ printalphabet/main.go
@@ -3,14 +3,8 @@ import ("github.com/01-edu/z01")
 
 func main(){
 
-	for i := 'a' ; i <= 'z' ; i++
-	{
-		z01.PrintRune(i)}
+	for i := 'a' ; i <= 'z' ; i++ {
+		z01.PrintRune(i)
+    }
 		z01.PrintRune('\n')
-
-
-
-
-
-
 }
