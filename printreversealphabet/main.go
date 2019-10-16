7699eca5e240da2c136881124ba995859c386849
aid000 <1efori9@gmail.com>
2019-10-16 13:55:13 +0600

    12

--- printreversealphabet/main.go
+++ printreversealphabet/main.go
@@ -1,13 +1,13 @@
 package main
 
 import (
-        "github.com/01-edu/z01"
+	"github.com/01-edu/z01"
 )
 
 func main() {
 
-        for i := 'z'; i >= 'a'; i-- {
-                z01.PrintRune(i)
-        }
-        z01.PrintRune('\n')
+	for i := 'z'; i >= 'a'; i-- {
+		z01.PrintRune(i)
+	}
+	z01.PrintRune('\n')
 }
