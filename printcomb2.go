21c70028be0adb2a923e85cb8e68bdcc6c239de0
aid000 <1efori9@gmail.com>
2019-10-16 22:52:34 +0600

    asdaass

--- printcomb2.go
+++ printcomb2.go
@@ -1,17 +1,15 @@
 package piscine
 
-import (
-        "github.com/01-edu/z01"
-)
+import ("github.com/01-edu/z01")
 
 func PrintComb2() {
 	
      
-	for a := '0'; a <= '9'; a = a + 1 {
-		for b := '0'; b <= '9'; b = b + 1 {
+	for a := '0'; a <= '9'; a++ {
+		for b := '0'; b <= '9'; b++ {
 			d := b + 1
-			for c := a; c <= '9'; c = c + 1 {
-				for ; d <= '9'; d = d + 1 {
+			for c := a; c <= '9'; c++ {
+				for ; d <= '9'; d++ {
 					z01.PrintRune(a)
 					z01.PrintRune(b)
 					z01.PrintRune(' ')
@@ -27,5 +25,5 @@ func PrintComb2() {
 			}
 		}
 	}
-	z01.PrintRune('\n')
+	z01.PrintRune(10)
 }
