8a625a34bc9d8738d9b3e1714c3fbb1da93138ef
aid000 <1efori9@gmail.com>
2019-10-16 20:42:53 +0600

    lk

--- printcomb.go
+++ printcomb.go
@@ -2,19 +2,23 @@ package piscine
 
 import "github.com/01-edu/z01"
 
-func PrintComb() {
-
-	for a := '0'; a <= '9'; a++ {
-		for b := a + 1; b <= '9'; b++ {
-			for c := b + 1; c <= '9'; c++ {
-				z01.PrintRune(a)
-				z01.PrintRune(b)
-				z01.PrintRune(c)
-				if a < 55 {
-					z01.PrintRune(44)
-					z01.PrintRune(32)
+func PrintComb2() {
+	for a := 0; a < 10; a=a+1 {
+		for b := 0 ; b < 10; b= b+1 {
+			for c := a; c < 10; c= c+1{
+				for d := d < 10; d = d + 1; d= b + 1; d := 0 {
+					z01.PrintRune(a)
+					z01.PrintRune(b)
+					z01.PrintRune(' ')
+					z01.PrintRune(c)
+					z01.PrintRune(d)
+					if a < 9 || b < 8 || c < 9 || d < 9{
+						z01.PrintRune(',')
+						z01.PrintRune(' ')
+					}
 				}
 			}
+
 		}
 	}
 	z01.PrintRune(10)
