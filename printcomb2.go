288833ec9cb0207cec479665f9bdd04bbf0a2bab
aid000 <1efori9@gmail.com>
2019-10-16 23:14:03 +0600

    asdaass

--- printcomb2.go
+++ printcomb2.go
@@ -1,30 +1,17 @@
 package piscine
 
-import (
-	"github.com/01-edu/z01"
-)
+import "fmt"
 
 func PrintComb2() {
-
-	for a := '0'; a <= '9'; a = a + 1 {
-		for b := '0'; b <= '9'; b = b + 1 {
-			d := b + 1
-			for c := a; c <= '9'; c = c + 1 {
-				for ; d <= '9'; d = d + 1 {
-					z01.PrintRune(a)
-					z01.PrintRune(b)
-					z01.PrintRune(' ')
-					z01.PrintRune(c)
-					z01.PrintRune(d)
-					if a < '9' || b < '8' || c < '9' || d < '9' {
-						z01.PrintRune(',')
-						z01.PrintRune(' ')
-					}
-
+	for i := 0; i <= 98; i++ {
+		for j := 1; j <= 99; j++ {
+			if i < j {
+				if i == 98 && j == 99 {
+					fmt.Printf("%02d %02d\n", i, j)
+				} else {
+					fmt.Printf("%02d %02d, ", i, j)
 				}
-				d = '0'
 			}
 		}
 	}
-	z01.PrintRune('\n')
 }
