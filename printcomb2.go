8d070dd60a23ac5decb7b5e3281510b153629b3d
aid000 <1efori9@gmail.com>
2019-10-16 23:31:19 +0600

    asd

--- printcomb2.go
+++ printcomb2.go
@@ -6,7 +6,6 @@ func PrintComb2() {
 
 	for a := '0'; a <= '9'; a = a + 1 {
 		for b := '0'; b <= '9'; b = b + 1 {
-
 			d := b + 1
 			for c := a; c <= '9'; c = c + 1 {
 				for ; d <= '9'; d = d + 1 {
@@ -19,7 +18,6 @@ func PrintComb2() {
 						z01.PrintRune(',')
 						z01.PrintRune(' ')
 					}
-
 				}
 				d = '0'
 			}
