1a826c03e38d2d62e8204cbb29a51839799296c7
aid000 <1efori9@gmail.com>
2019-10-16 15:41:48 +0600

    TF

--- isnegative.go
+++ isnegative.go
@@ -5,7 +5,7 @@ import ("github.com/01-edu/z01")
 func isnegative(nb int) {
 	if nb<0 { z01.PrintRune("'T'")
 	}
-	else { z01.PrintRune("'F'")
+	if nb>0 { z01.PrintRune("'F'")
 	}
 	z01.PrintRune(10)
 }
