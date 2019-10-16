3b91b2152fc6022a4de761fbf161b47a923dbab3
aid000 <1efori9@gmail.com>
2019-10-16 21:57:04 +0600

    asdaass

--- printcomb2.go
+++ printcomb2.go
@@ -1,12 +1,11 @@
 package piscine
 
 import (
-        "github.com/01-edu/z01"
+	"github.com/01-edu/z01"
 )
 
 func PrintComb2() {
-	
-     
+
 	for a := '0'; a <= '9'; a = a + 1 {
 		for b := '0'; b <= '9'; b = b + 1 {
 			d := b + 1
