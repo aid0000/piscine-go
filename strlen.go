edd12f2525a76a646da18b3d07a64cfcdb590864
aid000 <1efori9@gmail.com>
2019-10-17 18:06:12 +0600

    xz

--- strlen.go
+++ strlen.go
@@ -1,18 +1,18 @@
 package piscine
 
-func StrLen(str string) int{
+func StrLen(str string) int {
 	nombre := []byte(str)
 	compteur := 0
 	double := 1
 	for _, v := range nombre {
-		if v <= 127{
+		if v <= 127 {
 			compteur++
-		}else{
-			if double<2 {
+		} else {
+			if double < 2 {
 				compteur++
 				double++
-			}else{
-				double=1
+			} else {
+				double = 1
 			}
 		}
 	}
