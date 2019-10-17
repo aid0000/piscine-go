a423d7915d205acfc9e85b35dcc381dbda8d29cc
aid000 <1efori9@gmail.com>
2019-10-17 20:02:33 +0600

    strln

--- strlen.go
+++ strlen.go
@@ -1,20 +1,9 @@
-package piscine
+package strlen
 
 func StrLen(str string) int {
-	nombre := []byte(str)
-	compteur := 0
-	double := 1
-	for _, v := range nombre {
-		if v <= 127 {
-			compteur++
-		} else {
-			if double < 2 {
-				compteur++
-				double++
-			} else {
-				double = 1
-			}
-		}
+	var nb int = 0
+	for range str {
+		nb++
 	}
-	return compteur
+	return nb
 }
