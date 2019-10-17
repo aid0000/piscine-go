56f898e08a9ffb6a909a29cb18cefeabb3412fdf
aid0000 <54474773+aid0000@users.noreply.github.com>
2019-10-17 18:07:09 +0600

    Update strlen.go

--- strlen.go
+++ strlen.go
@@ -1,1 +1,20 @@
-echo "12"
+package piscine
+
+func StrLen(str string) int{
+	nombre := []byte(str)
+	compteur := 0
+	double := 1
+	for _, v := range nombre {
+		if v <= 127{
+			compteur++
+		}else{
+			if double<2 {
+				compteur++
+				double++
+			}else{
+				double=1
+			}
+		}
+	}
+	return compteur
+}
