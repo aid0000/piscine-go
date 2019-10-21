d7d89f1e14a3baeb4247e967132d520124cff3dd
aid000 <1efori9@gmail.com>
2019-10-21 12:31:08 +0600

    iter

--- iterativefactorial.go
+++ iterativefactorial.go
@@ -1,12 +1,14 @@
 package piscine
 
 func IterativeFactorial(nb int) int {
-	if nb <= 0 {
-		result == 0
+	if nb <= 0 && nb >= 20 {
+		result := 1
+		i := 1
+		for ; i <= nb; i++ {
+			result = result * i
+		}
+		return result	
+	} else {
+		return 0
 	}
-	result := 1
-	for i := 1; i <= nb; i++ {
-		result = result * i
-	}
-	return result
 }
