360c35278b6b6fe6c0bf9e2fa87cecc453267eea
aid000 <1efori9@gmail.com>
2019-10-22 11:47:22 +0600

    isp

---
+++ isprime.go
@@ -0,0 +1,25 @@
+package piscine
+
+func IsPrime(n int) bool {
+	if n < 0 {
+		return false
+	}
+
+	if n <= 3 {
+		return n > 1
+	}
+
+	if n%2 == 0 || n%3 == 0 {
+		return false
+	}
+
+	i := 5
+	for i*i <= n {
+		if n%i == 0 || n%(i+2) == 0 {
+			return false
+		}
+		i = i + 6
+	}
+
+	return true
+}
