6f113b6feb6f9045d291ca7ac97be4282a8f3b77
aid000 <1efori9@gmail.com>
2019-10-23 20:07:32 +0600

    index

--- index.go
+++ index.go
@@ -1,19 +1,34 @@
 package piscine
 
 func Index(s string, toFind string) int {
-	sRunes := []rune(s)
-	n := StrLen(s)
-	toFindRunes := []rune(toFind)
-	for i := range sRunes {
-		matches := true
-		for j, r2 := range toFindRunes {
-			if i+j >= n || sRunes[i+j] != r2 {
-				matches = false
-				break
+	a := []rune(s)
+	b := []rune(toFind)
+	x := 0
+	y := 0
+
+	for index := range a {
+		index = index
+		x++
+	}
+	for index := range b {
+		index = index
+		y++
+	}
+	if y == 0 {
+		return 0
+	}
+
+	for index, value := range a {
+		if value == b[0] && x >= y+index-1 {
+			run := 1
+			for i := 1; i < y; i++ {
+				if b[i] == a[index+i] {
+					run++
+				}
 			}
-		}
-		if matches {
-			return i
+			if run == y {
+				return index
+			}
 		}
 	}
 	return -1
